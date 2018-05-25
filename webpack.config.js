2b2b25beff99d37e5326e428ff8390ae17b6d4d9
Mike Cooper <mythmon@gmail.com>
2018-05-25 14:48:07 -0700

    Fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const GenerateJsonPlugin = require("generate-json-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const packageData = require("./package.json");
 const manifest = require("./extension/manifest.json");
@@ -12,11 +11,11 @@ module.exports = {
   mode: "development",
   devtool: "source-map",
   entry: {
-    content: "./extension/content/index.js"
+    content: "./extension/content/index.js",
   },
   output: {
     filename: "[name].js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
   },
   plugins: [
     new CopyWebpackPlugin([
@@ -26,42 +25,35 @@ module.exports = {
     ]),
     new HtmlWebpackPlugin({
       title: "Normandy Devtools",
-      filename: "content.html"
+      filename: "content.html",
     }),
-    new GenerateJsonPlugin(
-      "manifest.json",
-      manifest,
-      (key, value) => {
-        if (typeof value === 'string' && value.startsWith('$')) {
-          let parts = value.slice(1).split('.');
-          let object = packageData;
-          while (parts.length > 0) {
-            object = object[parts.pop()];
-          }
-          return object;
+    new GenerateJsonPlugin("manifest.json", manifest, (key, value) => {
+      if (typeof value === "string" && value.startsWith("$")) {
+        let parts = value.slice(1).split(".");
+        let object = packageData;
+        while (parts.length > 0) {
+          object = object[parts.pop()];
         }
-        return value;
-      },
-    ),
+        return object;
+      }
+      return value;
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.js$/,
         include: [path.resolve(__dirname, "./extension")],
-        use: "babel-loader"
+        use: "babel-loader",
       },
       {
         test: /\.less/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: ["css-loader", "less-loader"],
-        }),
+        use: ["style-loader", "css-loader", "less-loader"],
       },
       {
         test: /\.(png|ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-        loader: "file-loader"
-      }
-    ]
-  }
+        loader: "file-loader",
+      },
+    ],
+  },
 };
