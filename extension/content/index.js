2dc7eb6574103716313e9a9c324f4e4c4f68732b
Mike Cooper <mythmon@gmail.com>
2018-05-29 13:43:17 -0700

    Add routing, remove logs from UI

--- extension/content/index.js
+++ extension/content/index.js
@@ -1,35 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter, Route, NavLink, Redirect } from "react-router-dom";
 
 import "./style.less";
-import LogViewer from "./LogViewer";
 import RecipeViewer from "./RecipeViewer";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      messages: [],
-    };
-
-    this.handleLogMessage = this.handleLogMessage.bind(this);
     this.runNormandy = this.runNormandy.bind(this);
-  }
-
-  async componentDidMount() {
-    browser.experiments.normandy.onNormandyLog.addListener(
-      this.handleLogMessage,
-    );
-  }
-
-  componentWillUnmount() {
-    browser.experiments.normandy.onNormandyLog.removeListener(
-      this.handleLogMessage,
-    );
-  }
-
-  handleLogMessage(message) {
-    this.setState(({ messages }) => ({ messages: messages.concat([message]) }));
   }
 
   async runNormandy() {
@@ -37,30 +16,39 @@ class App extends React.Component {
   }
 
   render() {
-    const { messages } = this.state;
     return (
-      <div className="app">
-        <header>
-          <h1>Normandy Devtools</h1>
-        </header>
+      <HashRouter>
+        <div className="app">
+          <header>
+            <h1>Normandy Devtools</h1>
+            <nav>
+              <NavLink to="/recipes">Recipes</NavLink>
+            </nav>
+          </header>
 
-        <div className="content">
-          <section>
-            <h1>Controls</h1>
-            <button onClick={this.runNormandy}>Run Normandy</button>
-          </section>
-          <section>
-            <RecipeViewer />
-          </section>
+          <Route exact path="/" render={() => <Redirect to="/recipes" />} />
+          <Route
+            path="/recipes"
+            render={props => <Recipes runNormandy={this.runNormandy} />}
+          />
         </div>
-
-        <aside>
-          <h2>Log</h2>
-          <LogViewer messages={messages} />
-        </aside>
-      </div>
+      </HashRouter>
     );
   }
+}
+
+function Recipes({ runNormandy }) {
+  return (
+    <div className="content">
+      <section>
+        <h1>Controls</h1>
+        <button onClick={runNormandy}>Run Normandy</button>
+      </section>
+      <section>
+        <RecipeViewer />
+      </section>
+    </div>
+  );
 }
 
 let target = document.querySelector("#target");
