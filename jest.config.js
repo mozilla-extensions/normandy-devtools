394d001524bdcc3641b628b0bdfa5623c97a25cc
tiftran <ttran@mozilla.com>
2020-04-02 12:17:04 -0700

    config changes

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,7 @@ module.exports = {
   globals: {
     browser: {
       experiments: {},
-      identity: {
-        getRedirectURL: () => {},
-        launchWebAuthFlow: () => {
-          return "https://08e23a90be40fa842a9f18ac049d45a7c3c8e7ec.extensions.allizom.org/#access_token=abc&scope=openid%20profile%20email&expires_in=7200&token_type=Bearer&state=abc&id_token=abc";
-        },
-      },
+      identity: {},
     },
   },
   setupFiles: ["<rootDir>/tests/conftests.js"],
--- tests/conftests.js
+++ tests/conftests.js
@@ -1,6 +1,5 @@
 /* eslint-env node */
 import crypto from "crypto";
-
 Object.defineProperty(global.self, "crypto", {
   value: {
     getRandomValues: arr => crypto.randomBytes(arr.length),
@@ -10,12 +9,15 @@ browser.experiments.normandy = {
   checkRecipeFilter: () => null,
   runRecipe: () => null,
 };
-global.document.body.createTextRange = () => {
-  return {
-    setEnd: () => {},
-    setStart: () => {},
-    getBoundingClientRect: () => {},
-    getClientRects: () => [],
-  };
+browser.identity = {
+  getRedirectURL: () => {},
+  launchWebAuthFlow: () => {
+    return "https://08e23a90be40fa842a9f18ac049d45a7c3c8e7ec.extensions.allizom.org/#access_token=abc&scope=openid%20profile%20email&expires_in=7200&token_type=Bearer&state=abc&id_token=abc";
+  },
 };
-//Element.prototype.getClientRect = ()=>{return [0,0,0,0]};
+global.document.body.createTextRange = () => ({
+  setEnd: () => {},
+  setStart: () => {},
+  getBoundingClientRect: () => {},
+  getClientRects: () => [],
+});
