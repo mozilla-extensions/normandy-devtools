fdc78c69d85cfb2e328569898fa0ef0140fb51e7
tiftran <ttran@mozilla.com>
2020-04-23 00:47:04 -0700

    consolelog after refactor merge

--- extension/content/components/recipes/form/ActionArguments.js
+++ extension/content/components/recipes/form/ActionArguments.js
@@ -2,11 +2,14 @@ import React from "react";
 import { ControlLabel, FormGroup } from "rsuite";
 
 import JsonEditor from "devtools/components/common/JsonEditor";
+import ConsoleLog from "devtools/components/recipes/form/fields/ConsoleLog";
 import {
   ACTION_UPDATE_DATA,
   useRecipeDetailsData,
   useRecipeDetailsDispatch,
 } from "devtools/contexts/recipeDetails";
+
+const actionFieldsMapping = { "console-log": ConsoleLog };
 
 export default function ActionArguments() {
   const data = useRecipeDetailsData();
@@ -26,10 +29,16 @@ export default function ActionArguments() {
     return null;
   }
 
+  let ArgumentField = actionFieldsMapping[data.action.name];
+
+  if (!ArgumentField) {
+    ArgumentField = JsonEditor;
+  }
+
   return (
     <FormGroup>
       <ControlLabel>Action Arguments</ControlLabel>
-      <JsonEditor
+      <ArgumentField
         key={data.recipe ? data.recipe.id : "create"}
         value={data.arguments}
         onChange={handleChange}
--- extension/content/components/recipes/form/fields/ConsoleLog.js
+++ extension/content/components/recipes/form/fields/ConsoleLog.js
@@ -3,17 +3,18 @@ import PropTypes from "prop-types";
 import { Input, FormGroup, ControlLabel } from "rsuite";
 
 export default function ConsoleLog(props) {
+  const { value: actionArgs, onChange } = props;
   const handleChange = (value) => {
-    const message = { message: value };
-    props.onChange(message);
+    onChange({ ...actionArgs, message: value });
   };
+
   return (
     <FormGroup>
       <ControlLabel>Message</ControlLabel>
       <Input
-        name={"consoleLogArgument"}
-        value={props.value ? props.value.message : null}
-        onChange={(value) => handleChange(value)}
+        name="consoleLogArgument"
+        value={actionArgs ? actionArgs.message : null}
+        onChange={(newValue) => handleChange(newValue)}
       />
     </FormGroup>
   );
