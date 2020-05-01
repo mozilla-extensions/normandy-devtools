8265b2bd9c63a5b89a1a065a2e5d17982151d783
tiftran <ttran@mozilla.com>
2020-04-30 21:50:47 -0700

    add multipref args

--- extension/content/components/recipes/form/ActionArguments.js
+++ extension/content/components/recipes/form/ActionArguments.js
@@ -6,6 +6,7 @@ import BranchedAddon from "devtools/components/recipes/form/arguments/BranchedAd
 import PreferenceExperiment from "devtools/components/recipes/form/arguments/PreferenceExperiment";
 import MessagingExperiment from "devtools/components/recipes/form/arguments/MessagingExperiment";
 import ConsoleLog from "devtools/components/recipes/form/arguments/ConsoleLog";
+import MultiPreference from "devtools/components/recipes/form/arguments/MultiPreference";
 import OptOutStudy from "devtools/components/recipes/form/arguments/OptOutStudy";
 import PreferenceRollout from "devtools/components/recipes/form/arguments/PreferenceRollout";
 import PreferenceRollback from "devtools/components/recipes/form/arguments/PreferenceRollback";
@@ -15,6 +16,7 @@ const ARGUMENTS_FIELDS_MAPPING = {
   "branched-addon-study": BranchedAddon,
   "console-log": ConsoleLog,
   "messaging-experiment": MessagingExperiment,
+  "multi-preference-experiment": MultiPreference,
   "opt-out-study": OptOutStudy,
   "preference-experiment": PreferenceExperiment,
   "preference-rollback": PreferenceRollback,
@@ -35,6 +37,13 @@ export const INITIAL_ACTION_ARGUMENTS = {
     branches: [],
     isEnrollmentPaused: false,
     slug: "",
+  },
+  "multi-preference-experiment": {
+    branches: [],
+    experimentDocumentUrl: "",
+    slug: "",
+    userFacingDescription: "",
+    userFacingName: "",
   },
   "opt-out-study": {
     addonUrl: "",
---
+++ extension/content/components/recipes/form/arguments/MultiPreferenceBranches.js
@@ -0,0 +1,288 @@
+// @ts-nocheck
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  Col,
+  ControlLabel,
+  Divider,
+  FormGroup,
+  HelpBlock,
+  Icon,
+  IconButton,
+  Input,
+  InputNumber,
+  Panel,
+} from "rsuite";
+
+import {
+  ACTION_UPDATE_DATA,
+  useRecipeDetailsData,
+  useRecipeDetailsDispatch,
+} from "devtools/contexts/recipeDetails";
+import PreferenceFields from "devtools/components/recipes/form/arguments/PreferenceFields";
+
+export default function MultiPreferenceBranches() {
+  const data = useRecipeDetailsData();
+  const dispatch = useRecipeDetailsDispatch();
+
+  const handleClickAddBranch = () => {
+    /** @type {{ratio: number, slug: string, branches: array}} */
+    const newBranch = { ratio: 1, slug: "", preferences: {} };
+
+    dispatch({
+      type: ACTION_UPDATE_DATA,
+      data: {
+        ...data,
+        arguments: {
+          ...data.arguments,
+          branches: [...data.arguments.branches, newBranch],
+        },
+      },
+    });
+  };
+
+  let branchesList = <HelpBlock>There are no branches.</HelpBlock>;
+  if (data.arguments.branches && data.arguments.branches.length) {
+    branchesList = data.arguments.branches.map((branch, index) => (
+      <Branch key={index} index={index} />
+    ));
+  }
+
+  return (
+    <FormGroup>
+      <ControlLabel>Branches</ControlLabel>
+      <Panel bordered>
+        {branchesList}
+        <Divider />
+        <IconButton
+          icon={<Icon icon="plus-circle" />}
+          onClick={handleClickAddBranch}
+        >
+          Add Branch
+        </IconButton>
+      </Panel>
+    </FormGroup>
+  );
+}
+
+function Branch({ index }) {
+  const data = useRecipeDetailsData();
+  const dispatch = useRecipeDetailsDispatch();
+  const branch = data.arguments.branches[index];
+  const { branches } = data.arguments;
+  const [preferences, setPreferences] = React.useState([]);
+
+  React.useEffect(() => {
+    if (branch.preferences) {
+      const all_preferences = Object.keys(branch.preferences).map((pref) => {
+        return { ...branch.preferences[pref], preferenceName: pref };
+      });
+      setPreferences(all_preferences);
+    }
+  }, []);
+
+  const handleClickAddPref = () => {
+    const newPref = {
+      preferenceName: "",
+      preferenceValue: "",
+      preferenceBranchType: "",
+      preferenceType: "",
+    };
+    setPreferences([...preferences, newPref]);
+  };
+
+  const handleDeletePref = (prefIndex, name) => {
+    if (name) {
+      /* eslint-disable no-unused-vars */
+      const { [name]: _omitPref, ...cleanedPref } = branch.preferences;
+      /* eslint-enable no-unused-vars */
+
+      handlePrefDataUpdate(cleanedPref);
+    }
+
+    setPreferences(preferences.filter((p, i) => i !== prefIndex));
+  };
+
+  const handlePrefChange = (prefIndex, name, value) => {
+    if (name === "preferenceName") {
+      handlePrefNameChange(prefIndex, value);
+    } else {
+      handlePrefFieldChange(prefIndex, name, value);
+    }
+  };
+
+  const handlePrefFieldChange = (prefIndex, prefFieldName, prefValue) => {
+    const preference = preferences[prefIndex];
+    const { preferenceName, ...prefValues } = preference;
+    let updatedPref;
+    if (prefFieldName === "preferenceType") {
+      updatedPref = {
+        ...prefValues,
+        [prefFieldName]: prefValue,
+        preferenceValue: "",
+      };
+    } else {
+      updatedPref = { ...prefValues, [prefFieldName]: prefValue };
+    }
+
+    /* eslint-disable no-unused-vars */
+    const {
+      [preferenceName]: _omitPrefValue,
+      ...cleanedPref
+    } = branch.preferences;
+    /* eslint-enable no-unused-vars */
+
+    const updatedPreferences = {
+      ...cleanedPref,
+      [preferenceName]: updatedPref,
+    };
+    handlePrefDataUpdate(updatedPreferences);
+
+    preferences[prefIndex] = { preferenceName, ...updatedPref };
+    setPreferences(preferences);
+  };
+
+  const handlePrefNameChange = (prefIndex, newPrefName) => {
+    const preference = preferences[prefIndex];
+    const { preferenceName: oldPrefName, ...prefValues } = preference;
+    /* eslint-disable no-unused-vars */
+    const { [oldPrefName]: _omitPrefName, ...cleanedPref } = branch.preferences;
+    /* eslint-enable no-unused-vars */
+    const updatedPref = { ...cleanedPref, [newPrefName]: prefValues };
+    handlePrefDataUpdate(updatedPref);
+    preference.preferenceName = newPrefName;
+    setPreferences(preferences);
+  };
+
+  const handlePrefDataUpdate = (updatedPref) => {
+    dispatch({
+      type: ACTION_UPDATE_DATA,
+      data: {
+        ...data,
+        arguments: {
+          ...data.arguments,
+          branches: branches.map((b, i) => {
+            if (i === index) {
+              return {
+                ...b,
+                preferences: updatedPref,
+              };
+            }
+
+            return b;
+          }),
+        },
+      },
+    });
+  };
+
+  const handleClickDelete = () => {
+    dispatch({
+      type: ACTION_UPDATE_DATA,
+      data: {
+        ...data,
+        arguments: {
+          ...data.arguments,
+          branches: branches.filter((b, i) => i !== index),
+        },
+      },
+    });
+  };
+
+  const handleChange = (name) => {
+    return (value) => {
+      dispatch({
+        type: ACTION_UPDATE_DATA,
+        data: {
+          ...data,
+          arguments: {
+            ...data.arguments,
+            branches: branches.map((b, i) => {
+              if (i === index) {
+                return { ...b, [name]: value };
+              }
+
+              return b;
+            }),
+          },
+        },
+      });
+    };
+  };
+
+  let prefList = <HelpBlock>There are no preferences.</HelpBlock>;
+  if (branch && branch.preferences) {
+    prefList = preferences.map((pref, i) => (
+      <PreferenceFields
+        key={i}
+        index={i}
+        prefData={pref}
+        onChange={handlePrefChange}
+        onDelete={handleDeletePref}
+      />
+    ));
+  }
+
+  const parseNumericInput = (value) => {
+    if (!value) {
+      return "";
+    }
+
+    return parseInt(value, 10);
+  };
+
+  return (
+    <FormGroup>
+      <div className="d-flex">
+        <div className="pr-2">
+          <ControlLabel>&nbsp;</ControlLabel>
+          <IconButton
+            circle
+            color="red"
+            icon={<Icon icon="trash" />}
+            size="sm"
+            onClick={handleClickDelete}
+          />
+        </div>
+        <div className="flex-grow-1 pr-1">
+          <FormGroup>
+            <ControlLabel>Branch Name</ControlLabel>
+            <Input value={branch.slug} onChange={handleChange("slug")} />
+          </FormGroup>
+        </div>
+        <div className="pr-1">
+          <FormGroup>
+            <ControlLabel>Ratio</ControlLabel>
+            <InputNumber
+              min={1}
+              style={{
+                width: "80px",
+              }}
+              value={branch.ratio}
+              onChange={handleChange("ratio", parseNumericInput)}
+            />
+          </FormGroup>
+        </div>
+      </div>
+      <Col xsOffset={1}>
+        <FormGroup>
+          <ControlLabel>Preferences</ControlLabel>
+          <Panel bordered>
+            {prefList}
+            <Divider />
+            <IconButton
+              icon={<Icon icon="plus-circle" />}
+              onClick={handleClickAddPref}
+            >
+              Add Preference
+            </IconButton>
+          </Panel>
+        </FormGroup>
+      </Col>
+    </FormGroup>
+  );
+}
+
+Branch.propTypes = {
+  index: PropTypes.number,
+};
