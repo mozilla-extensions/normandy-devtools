64b989682686e06ffbd7fae1fdf34982eb13ef78
Rehan Dalal <rehandalal@gmail.com>
2020-10-08 18:30:48 -0400

    Add test for HideFromTests

--- content/tests/components/common/HideFromTests.test.tsx
+++ content/tests/components/common/HideFromTests.test.tsx
@@ -3,12 +3,32 @@ import React from "react";
 
 import { HideFromTests } from "devtools/components/common/HideFromTests";
 
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace NodeJS {
+    interface Global {
+      __TESTING__: boolean;
+    }
+  }
+}
+
 describe("HideFromTests", () => {
   afterEach(async () => {
+    global.__TESTING__ = true;
     await cleanup();
   });
 
-  it("should hide it's children", async () => {
+  it("show not hide it's children when testing", () => {
+    global.__TESTING__ = false;
+    const { getByText } = render(
+      <HideFromTests>
+        <div>SHOW ME!</div>
+      </HideFromTests>,
+    );
+    expect(getByText("SHOW ME!")).toBeInTheDocument();
+  });
+
+  it("should hide it's children when testing", () => {
     const { queryByText } = render(
       <HideFromTests>
         <div>SHOW ME!</div>
