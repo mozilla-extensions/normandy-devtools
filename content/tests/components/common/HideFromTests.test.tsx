9793dba41bf558f41b1ca5aa92dbffc9277884a3
Rehan Dalal <rehandalal@gmail.com>
2020-10-08 01:40:01 -0400

    Cleanup after tests

--- content/tests/components/common/HideFromTests.test.tsx
+++ content/tests/components/common/HideFromTests.test.tsx
@@ -1,9 +1,13 @@
-import { render } from "@testing-library/react";
+import { cleanup, render } from "@testing-library/react";
 import React from "react";
 
 import { HideFromTests } from "devtools/components/common/HideFromTests";
 
 describe("HideFromTests", () => {
+  afterEach(async () => {
+    await cleanup();
+  });
+
   it("should hide it's children", async () => {
     const { queryByText } = render(
       <HideFromTests>
