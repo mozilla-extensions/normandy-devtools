0e28c749b119100761ec870f4db3f65017bb6a98
Mike Cooper <mythmon@gmail.com>
2020-10-14 16:30:10 -0700

    Rewrite RecipeListing as a TSX functional component, with no cache

--- content/components/pages/RecipesPage.tsx
+++ content/components/pages/RecipesPage.tsx
@@ -1,6 +1,4 @@
-import autobind from "autobind-decorator";
-import PropTypes from "prop-types";
-import React, { ReactElement } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import {
   Button,
@@ -31,7 +29,7 @@ import { convertToV1Recipe } from "devtools/utils/recipes";
 
 const normandy = browser.experiments.normandy;
 
-interface Props {
+interface RecipesPageProps {
   api: NormandyAPI;
   connectionStatus: boolean;
   environment: Environment;
@@ -39,275 +37,97 @@ interface Props {
   environments: Record<string, Environment>;
 }
 
-interface State {
-  arbitraryRecipe: string;
-  count: number;
-  loading: boolean;
-  page: number;
-  runningArbitrary: boolean;
-  showWriteRecipes: boolean;
-  recipeSelected: boolean;
-  recipePages: Record<string, Record<number, Array<RecipeV3>>>;
-}
+// Wrapped in a HOC and then exported as default
+const RecipesPage: React.FC<RecipesPageProps> = ({
+  environmentKey,
+  api,
+  connectionStatus,
+}) => {
+  const [currentPageRecipes, setCurrentPageRecipes] = useState([]);
+  const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [pageNum, setPageNum] = useState(1);
 
-@autobind
-class RecipesPage extends React.PureComponent<Props, State> {
-  static propTypes = {
-    api: PropTypes.object,
-    connectionStatus: PropTypes.bool,
-    environment: PropTypes.object,
-    environmentKey: PropTypes.string,
-    environments: PropTypes.object,
-  };
+  const [arbitraryRecipe, setArbitraryRecipe] = useState("");
+  const [showWriteRecipes, setShowWriteRecipes] = useState(false);
 
-  constructor(props) {
-    super(props);
+  // Load recipes
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      const data = await api.fetchRecipePage(pageNum, { ordering: "-id" });
+      setCurrentPageRecipes(data.results);
+      setCount(data.count);
+      setLoading(false);
+    })();
+  }, [pageNum, environmentKey, connectionStatus]);
 
-    const recipePages = {};
-    Object.keys(props.environments).forEach((v) => {
-      recipePages[v] = {};
-    });
-
-    this.state = {
-      arbitraryRecipe: "",
-      count: 0,
-      loading: false,
-      page: 1,
-      runningArbitrary: false,
-      showWriteRecipes: false,
-      recipeSelected: false,
-      recipePages,
-    };
-  }
-
-  async runNormandy(): Promise<void> {
-    await normandy.standardRun();
-  }
-
-  async componentDidMount(): Promise<void> {
-    const { page } = this.state;
-    const { environment } = this.props;
-    this.refreshRecipeList(environment, page);
-  }
-
-  async componentDidUpdate(prevProps): Promise<void> {
-    const { connectionStatus, environmentKey, environment } = this.props;
-    if (
-      environmentKey !== prevProps.environmentKey ||
-      connectionStatus !== prevProps.connectionStatus
-    ) {
-      this.refreshRecipeList(environment, this.state.page);
-    }
-  }
-
-  async refreshRecipeList(environment, page): Promise<void> {
-    const { api, environmentKey } = this.props;
-    if (
-      environmentKey in this.state.recipePages &&
-      page in this.state.recipePages[environmentKey]
-    ) {
-      // cache hit
-      this.setState({ page });
-      return;
-    }
-
-    // cache miss
-    this.setState({ loading: true });
-    const data = await api.fetchRecipePage(page, {
-      ordering: "-id",
-    });
-
-    this.setState(({ recipePages }) => ({
-      recipePages: {
-        ...recipePages,
-        [environmentKey]: {
-          ...recipePages[environmentKey],
-          [page]: data.results,
-        },
-      },
-      page,
-      loading: false,
-      count: data.count,
-    }));
-  }
-
-  handlePageChange(page): void {
-    const { environment } = this.props;
-    this.refreshRecipeList(environment, page);
-  }
-
-  copyRecipeToArbitrary(v3Recipe): void {
-    const { environmentKey } = this.props;
+  function copyRecipeToArbitrary(v3Recipe): void {
     const v1Recipe = convertToV1Recipe(
       v3Recipe.latest_revision,
       environmentKey,
     );
-    this.setState({
-      arbitraryRecipe: JSON.stringify(v1Recipe, null, 2),
-      showWriteRecipes: true,
-    });
+    setArbitraryRecipe(JSON.stringify(v1Recipe, null, 2));
+    setShowWriteRecipes(true);
   }
 
-  renderRecipeList(): ReactElement {
-    const { loading, page, recipePages } = this.state;
-    const { environmentKey } = this.props;
-    const { [environmentKey]: { [page]: recipes = [] } = {} } = recipePages;
-    if (loading) {
-      return (
-        <div className="text-center">
-          <Loader content="Loading recipes&hellip;" />
-        </div>
-      );
-    } else if (recipes) {
-      return (
-        <Grid className="recipe-list">
-          {chunkBy(recipes, 2).map((recipeChunk, rowIdx) => (
-            <Row key={`row-${rowIdx}`}>
-              {recipeChunk.map((recipe, colIdx) => (
-                <Col key={`col-${colIdx}`} md={12} sm={24}>
-                  <RecipeListing
-                    key={recipe.id}
-                    copyRecipeToArbitrary={this.copyRecipeToArbitrary}
-                    environmentName={environmentKey}
-                    recipe={recipe}
-                  />
-                </Col>
-              ))}
-            </Row>
-          ))}
-        </Grid>
-      );
-    }
+  return (
+    <>
+      <Header>
+        <Navbar>
+          <Nav pullRight>
+            <Nav.Item
+              componentClass={Link}
+              icon={<Icon icon="edit" />}
+              to={`/${environmentKey}/recipes/new`}
+            >
+              Create Recipe
+            </Nav.Item>
+            <Nav.Item
+              icon={<Icon icon="edit" />}
+              onClick={() => setShowWriteRecipes(true)}
+            >
+              Write & Run Arbitrary
+            </Nav.Item>
+            <RunButton environmentKey={environmentKey} />
+          </Nav>
+        </Navbar>
+      </Header>
 
-    return null;
-  }
-
-  showWriteRecipePopup(): void {
-    this.setState({ showWriteRecipes: true });
-  }
-
-  hideWriteRecipePopup(): void {
-    this.setState({ showWriteRecipes: false });
-  }
-
-  handleArbitraryRecipeChange(editor, data, value): void {
-    this.setState({ arbitraryRecipe: value });
-  }
-
-  async runArbitraryRecipe(): Promise<void> {
-    const { arbitraryRecipe } = this.state;
-    this.setState({ runningArbitrary: true });
-    try {
-      await normandy.runRecipe(JSON.parse(arbitraryRecipe));
-    } finally {
-      this.setState({ runningArbitrary: false });
-    }
-  }
-
-  renderWriteRecipeModal(): ReactElement {
-    const { arbitraryRecipe, runningArbitrary } = this.state;
-
-    return (
-      <Modal
-        show={this.state.showWriteRecipes}
-        size="lg"
-        onHide={this.hideWriteRecipePopup}
-      >
-        <Modal.Header>
-          <Modal.Title>Write a recipe</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <CodeMirror
-            options={{
-              mode: "javascript",
-              lineNumbers: true,
-            }}
-            value={arbitraryRecipe}
-            onBeforeChange={this.handleArbitraryRecipeChange}
+      <div className="page-wrapper">
+        <RecipeList
+          copyRecipeToArbitrary={copyRecipeToArbitrary}
+          environmentKey={environmentKey}
+          loading={loading}
+          recipes={currentPageRecipes}
+        />
+        <div>
+          <Pagination
+            boundaryLinks
+            ellipsis
+            next
+            prev
+            activePage={pageNum}
+            maxButtons={5}
+            pages={Math.ceil(count / 25)}
+            size="lg"
+            onSelect={setPageNum}
           />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button
-            appearance="primary"
-            disabled={runningArbitrary}
-            onClick={this.runArbitraryRecipe}
-          >
-            Run
-          </Button>
-          <Button appearance="subtle" onClick={this.hideWriteRecipePopup}>
-            Cancel
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
-
-  renderRunButton(): ReactElement {
-    const { environmentKey } = this.props;
-    if (environmentKey !== "prod") {
-      return null;
-    }
-
-    return (
-      <Nav.Item icon={<Icon icon="play" />} onClick={this.runNormandy}>
-        Run Normandy
-      </Nav.Item>
-    );
-  }
-
-  render(): ReactElement {
-    const { page, count } = this.state;
-    const { environmentKey } = this.props;
-
-    return (
-      <>
-        <Header>
-          <Navbar>
-            <Nav pullRight>
-              <Nav.Item
-                componentClass={Link}
-                icon={<Icon icon="edit" />}
-                to={`/${environmentKey}/recipes/new`}
-              >
-                Create Recipe
-              </Nav.Item>
-              <Nav.Item
-                icon={<Icon icon="edit" />}
-                onClick={this.showWriteRecipePopup}
-              >
-                Write & Run Arbitrary
-              </Nav.Item>
-              {this.renderRunButton()}
-            </Nav>
-          </Navbar>
-        </Header>
-
-        <div className="page-wrapper">
-          {this.renderRecipeList()}
-          <div>
-            <Pagination
-              boundaryLinks
-              ellipsis
-              first
-              last
-              next
-              prev
-              activePage={page}
-              maxButtons={5}
-              pages={Math.ceil(count / 25)}
-              size="lg"
-              onSelect={this.handlePageChange}
-            />
-          </div>
         </div>
+      </div>
 
-        {this.renderWriteRecipeModal()}
-      </>
-    );
-  }
-}
+      <WriteRecipeModal
+        arbitraryRecipe={arbitraryRecipe}
+        setArbitraryRecipe={setArbitraryRecipe}
+        setShowWriteRecipes={setShowWriteRecipes}
+        showWriteRecipes={showWriteRecipes}
+      />
+    </>
+  );
+};
 
-export default function WrappedRecipePage(): ReactElement {
+// export default
+const WrappedRecipePage: React.FC = () => {
   const {
     connectionStatus,
     environment,
@@ -324,4 +144,116 @@ export default function WrappedRecipePage(): ReactElement {
       environments={environments}
     />
   );
-}
+};
+
+export default WrappedRecipePage;
+
+const RecipeList: React.FC<{
+  loading: boolean;
+  recipes: Array<RecipeV3>;
+  copyRecipeToArbitrary: (recipe: RecipeV3) => void;
+  environmentKey: string;
+}> = ({ loading, recipes, copyRecipeToArbitrary, environmentKey }) => {
+  if (loading) {
+    return (
+      <div className="text-center">
+        <Loader content="Loading recipes&hellip;" />
+      </div>
+    );
+  } else if (recipes) {
+    return (
+      <Grid className="recipe-list">
+        {chunkBy(recipes, 2).map((recipeChunk, rowIdx) => (
+          <Row key={`row-${rowIdx}`}>
+            {recipeChunk.map((recipe, colIdx) => (
+              <Col key={`col-${colIdx}`} md={12} sm={24}>
+                <RecipeListing
+                  key={recipe.id}
+                  copyRecipeToArbitrary={copyRecipeToArbitrary}
+                  environmentName={environmentKey}
+                  recipe={recipe}
+                />
+              </Col>
+            ))}
+          </Row>
+        ))}
+      </Grid>
+    );
+  }
+
+  return null;
+};
+
+const RunButton: React.FC<{
+  environmentKey: string;
+}> = ({ environmentKey }) => {
+  if (environmentKey !== "prod") {
+    return null;
+  }
+
+  return (
+    <Nav.Item
+      icon={<Icon icon="play" />}
+      onClick={() => normandy.standardRun()}
+    >
+      Run Normandy
+    </Nav.Item>
+  );
+};
+
+const WriteRecipeModal: React.FC<{
+  arbitraryRecipe: string;
+  showWriteRecipes: boolean;
+  setShowWriteRecipes: (show: boolean) => void;
+  setArbitraryRecipe: (newRecipe: string) => void;
+}> = ({
+  arbitraryRecipe,
+  setShowWriteRecipes,
+  showWriteRecipes,
+  setArbitraryRecipe,
+}) => {
+  const [runningArbitrary, setRunningArbitrary] = useState(false);
+
+  async function runArbitraryRecipe(): Promise<void> {
+    setRunningArbitrary(true);
+    try {
+      await normandy.runRecipe(JSON.parse(arbitraryRecipe));
+    } finally {
+      setRunningArbitrary(false);
+    }
+  }
+
+  return (
+    <Modal
+      show={showWriteRecipes}
+      size="lg"
+      onHide={() => setShowWriteRecipes(false)}
+    >
+      <Modal.Header>
+        <Modal.Title>Write a recipe</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <CodeMirror
+          options={{
+            mode: "javascript",
+            lineNumbers: true,
+          }}
+          value={arbitraryRecipe}
+          onBeforeChange={(editor, data, value) => setArbitraryRecipe(value)}
+        />
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          appearance="primary"
+          disabled={runningArbitrary}
+          onClick={runArbitraryRecipe}
+        >
+          Run
+        </Button>
+        <Button appearance="subtle" onClick={() => setShowWriteRecipes(false)}>
+          Cancel
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
